d00117fac02ddfcb0b915a3975469ebd1c045d6a
Matteopast01 <matteopast@gmail.com>
2023-11-06 16:37:41 +0100

    Little Correction in BeerApiHandler

--- services/BeerApi/BeerApiHandler.js
+++ services/BeerApi/BeerApiHandler.js
@@ -1,15 +1,15 @@
 
 
 async function requestBeersByName(name, postprocessing = ()=>{}) {
-    await result = fetch(`https://api.punkapi.com/v2/beers/${name}`)
-    await json = result.json()
+     result = await fetch(`https://api.punkapi.com/v2/beers/${name}`)
+     json = await result.json()
     postprocessing(json)
 
 }
 
 async function requestBeersById(id, postprocessing = ()=>{}) {
-    await result = fetch(`https://api.punkapi.com/v2/beers/${id}`)
-    await json = result.json()
+    result = await fetch(`https://api.punkapi.com/v2/beers/${id}`)
+    json = await result.json()
     postprocessing(json)
 
 
