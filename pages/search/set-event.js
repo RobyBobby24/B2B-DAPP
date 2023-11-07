ace381f8d06c6d1ff1c112ecefbd900971d78f10
Roberto Di Stefano <robertodistefano24@gmail.com>
2023-11-07 13:55:00 +0100

    sistemato setevent search

--- pages/search/set-event.js
+++ pages/search/set-event.js
@@ -1,5 +1,7 @@
-for(let i=1 ; i<=5; i++)
+for(let i=1 ; i<=5; i++){
     document.getElementById( `element${i}search`).addEventListener("mouseover", () => {
         document.getElementById( `element${i}search`).style.filter = 'brightness(50%)'})
     document.getElementById( `element${i}search`).addEventListener("mouseout", () => {
-        document.getElementById( `element${i}search`).style.filter = 'brightness(0%)'})
+        document.getElementById( `element${i}search`).style.filter = 'brightness(100%)'})
+}
+
