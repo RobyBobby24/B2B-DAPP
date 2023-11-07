8bc1bd97cbec26da792cd096e2108497aed18c4a
Matteopast01 <matteopast@gmail.com>
2023-11-07 13:14:48 +0100

    event for js

---
+++ pages/search/set-event.js
@@ -0,0 +1,5 @@
+for(let i=1 ; i<=5; i++)
+    document.getElementById( `element${i}search`).addEventListener("mouseover", () => {
+        document.getElementById( `element${i}search`).style.filter = 'brightness(50%)'})
+    document.getElementById( `element${i}search`).addEventListener("mouseout", () => {
+        document.getElementById( `element${i}search`).style.filter = 'brightness(0%)'})
