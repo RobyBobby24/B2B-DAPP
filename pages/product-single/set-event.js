89dfe04c02bffc77b699fbad659609ac159d75d9
giacomo.alfani@student.univaq.it <giacomo.alfani@student.univaq.it>
2023-11-08 10:54:05 +0100

    modified product-single to load correctly the beers

--- pages/product-single/set-event.js
+++ pages/product-single/set-event.js
@@ -1,6 +1,15 @@
 import {delete_rew, load_rew, store_rew, update_rew} from "../../services/firebase/firestore-database/crud-op.js";
 import {get_review, get_id, set_review, set_beer} from "./utility-function.js"
 
+// for loading beers by id in product-single.html
+document.addEventListener("DOMContentLoaded", ()=>{
+    let arrayOfId = [123,192,40]
+    for (let i=1 ; i<=3; i++){
+        set_beer(arrayOfId[i-1], `product-${i}-img`, `product-${i}-name`, `product-${i}-property`)
+    }
+})
+
+/*
 document.getElementById( "store").addEventListener("click",()=> {
     store_rew(get_review, "Review")
 })
@@ -16,11 +25,6 @@ document.getElementById( "update").addEventListener("click", ()=> {
 document.getElementById( "delete").addEventListener("click", ()=>{
     delete_rew("Review",get_id)
 })
+*/
 
-// for loading beers by id in product-single.html
-document.addEventListener("DOMContentLoaded", ()=>{
-    let arrayOfId = [123,192,40]
-    for (let i=1 ; i<=3; i++){
-        set_beer(arrayOfId[i-1], `product-${i}-img`, `product-${i}-name`, `product-${i}-property`)
-    }
-})
+
