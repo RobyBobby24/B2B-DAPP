82e4b5d0e497211d51276c319b9de3efcef12add
Martak9 <marta.paciotti@student.univaq.it>
2023-11-08 10:13:07 +0100

    modified set-event.js and utility-function.js in product-single

--- pages/product-single/set-event.js
+++ pages/product-single/set-event.js
@@ -1,5 +1,5 @@
 import {delete_rew, load_rew, store_rew, update_rew} from "../../services/firebase/firestore-database/crud-op.js";
-import {get_review, get_id, set_review} from "./utility-function"
+import {get_review, get_id, set_review, set_beer} from "./utility-function.js"
 
 document.getElementById( "store").addEventListener("click",()=> {
     store_rew(get_review, "Review")
@@ -15,4 +15,12 @@ document.getElementById( "update").addEventListener("click", ()=> {
 
 document.getElementById( "delete").addEventListener("click", ()=>{
     delete_rew("Review",get_id)
+})
+
+// for loading beers by id in product-single.html
+document.addEventListener("DOMContentLoaded", ()=>{
+    let arrayOfId = [123,192,40]
+    for (let i=1 ; i<=3; i++){
+        set_beer(arrayOfId[i-1], `product-${i}-img`, `product-${i}-name`, `product-${i}-property`)
+    }
 })
